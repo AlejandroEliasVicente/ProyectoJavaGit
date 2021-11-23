fb9c485b471765cff1e91e4ff73a5fd3711311ae
Alex <Alex@Alex.com>
2021-11-23 10:21:38 +0100

    Tercer commit con Main

--- src/es/centroafuera/Main.java
+++ src/es/centroafuera/Main.java
@@ -3,6 +3,7 @@ package es.centroafuera;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hola")
+	System.out.println("Hola");
+        System.out.println("Que tal estas?");
     }
 }
